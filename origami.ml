8d18f38c7a359a433c6efa712b298d4c168ea0e3
TheLKL321 <lkl.zarebski@gmail.com>
2017-12-10 16:45:41 +0100

    add prosta, prostopadla, przeciecie

--- origami.ml
+++ origami.ml
@@ -1,38 +1,57 @@
 open List;;
 
-(** Punkt na płaszczyźnie *)
+(*  Punkt na płaszczyźnie *)
 type point = float * float
 
-(** Poskładana kartka: ile razy kartkę przebije szpilka wbita w danym punkcie *)
+(*  Poskładana kartka: ile razy kartkę przebije szpilka wbita w danym punkcie *)
 type kartka = point -> int
 
-(** Zwraca kartkę, reprezentującą domknięty prostokąt
+(*  Zwraca kartkę, reprezentującą domknięty prostokąt
     o bokach równoległych do osi układu współrzędnych i lewym dolnym rogu [p1]
     a prawym górnym [p2]. Punkt [p1] musi więc być nieostro na lewo i w dół
     od punktu [p2]. Gdy w kartkę tę wbije się szpilkę wewnątrz
     (lub na krawędziach) prostokąta, kartka zostanie przebita 1 raz,
     w pozostałych przypadkach 0 razy *)
 let prostokat (x1, y1) (x2, y2) =
-  let kartka (x, y) =
+  fun (x, y) ->
     if x >= x1 && x <= x2 && y >= y1 && y <= y2 then 1
     else 0
 
-(** Zwraca kwadrat danej liczby  *)
-let sq x = x * x
+(*  Zwraca kwadrat danej liczby  *)
+let sq x = x *. x
 
-(** Zwraca kartkę, reprezentującą kółko domknięte o środku w punkcie [p] i promieniu [r] *)
+(*  Zwraca kartkę, reprezentującą kółko domknięte o środku w punkcie [p] i
+    promieniu [r] *)
 let kolko (xs, ys) r =
-  let kartka (x, y) =
-    if (sq (x -. xs)) + (sq (y -. ys)) <= sq r then 1
+  fun (x, y) ->
+    if (sq (x -. xs)) +. (sq (y -. ys)) <= sq r then 1
     else 0
 
-(** Zwraca prostą zawierającą różne od siebie punkty p1 i p2  *)
-let prosta p1 p2 =
+(*  Zwraca funkcję przechodzącą przez punkt p, będącą prostą prostopadłą do
+    prostej o współczynniku kierunkowym a *)
+let prostopadla p a =
+  let kierunek = -1. /. a
+  in (kierunek, -.(kierunek *. fst p) +. snd p)
 
-(** Zwraca symetryczne odbicie punktu p względem prostej prost  *)
-let symetria p prost =
+(*  Zał: 
+    Zwraca parę o postaci (współczynnik kierunkowy, wyraz wolny) funkcji będącej
+    prostą zawierającą punkty p1 i p2  *)
+let prosta p1 p2 = 
+  let kierunek = (snd p2 -. snd p1)/.(fst p2 -. fst p1)
+  in (kierunek, -.(kierunek *. fst p1) +. snd p1)
 
-(** Składa kartkę [k] wzdłuż prostej przechodzącej prze
+(*  Zwraca punkt leżący na przecięciu prostych prost1 i prost2 *)
+let przeciecie prost1 prost2 = 
+  let x = (snd prost2 -. snd prost1) /. (fst prost1 -. fst prost2)
+  in 
+    let y = fst prost1 *. x +. snd prost1
+    in
+      (x, y) 
+
+(*  Zwraca symetryczne odbicie punktu p względem prostej prost  *)
+let symetria p prost = 42
+
+(*  Składa kartkę [k] wzdłuż prostej przechodzącej prze
     punkty [p1] i [p2] (muszą to być różne punkty). Papier jest składany
     w ten sposób, że z prawej strony prostej (patrząc w kierunku od [p1] do [p2])
     jest przekładany na lewą. Wynikiem funkcji jest złożona kartka. Jej
@@ -43,20 +62,21 @@ let symetria p prost =
     który nałożył się na punkt przebicia. *)
 let zlozPara k ((x1, y1), (x2, y2)) =
   if x1 = x2 then
-    (* pion *)
+    (* pion *) 42
     (* TODO: JAK JESTEŚ PO ZŁEJ STRONIE PROSTEJ TO NIE ODBIJASZ *)
+  else if y1 = y2 then
+    (*  poziom *) 42
   else if x1 < x2 then
-    (* mniejsze od funkcji *)
+    (* mniejsze od funkcji *) 42
   else
-    (* większe od funkcji *)
+    (* większe od funkcji *) 42
 
-
-(** Wywołuje zlozPara odpowiednio dopasowując argumenty *)
+(*  Wywołuje zlozPara odpowiednio dopasowując argumenty *)
 let zloz p1 p2 k =
   zlozPara k (p1, p2)
 
-(** Wynikiem jest złożenie kartki [k] kolejno wzdłuż wszystkich prostych
-z listy l *)
+(*  Wynikiem jest złożenie kartki [k] kolejno wzdłuż wszystkich prostych
+    z listy l *)
 let skladaj l k =
   fold_left zlozPara k l
 ;;
